82f521f2f8f57c64d8a898e467c1a11bd81d03c4
ilias-01 <ilyas.mewa@gmail.com>
2024-12-05 18:29:52 -0500

    product list

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
+         <div className="landing_content" >
          <h1>Welcome To Paradise Nursery</h1>
           <div className="divider"></div>
           <p>Where Green Meets Serenity</p>
